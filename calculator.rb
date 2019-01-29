477d3a3bf2434319fe72211fbb9b8ce4a627d0b1
Sharmanidhi989 <nidhi.sharma2015@sait.ac.in>
2019-01-29 13:55:04 +0530

    now calculator.rb performs subtraction also

--- calculator.rb
+++ calculator.rb
@@ -6,3 +6,10 @@ puts 'please enter another number:'
 b = gets.to_i
 
 puts "Sum : #{a + b}"
+
+puts 'please enter a number:'
+a = gets.to_i
+puts 'please enter another number:'
+b = gets.to_i
+
+puts "Diffrence : #{a - b}"
